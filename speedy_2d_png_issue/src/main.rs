934594af4c193bb884022e85a55c52ae51f4da65
Webb Hinton <38958118+wyhinton@users.noreply.github.com>
2021-04-02 01:23:44 -0400

    Update main.rs

--- speedy_2d_png_issue/src/main.rs
+++ speedy_2d_png_issue/src/main.rs
@@ -25,11 +25,17 @@ impl WindowHandler for MyWindowHandler
     {
         graphics.clear_screen(Color::WHITE);
         let img: DynamicImage = ImageReader::open("imgs/smiley.png").unwrap().decode().unwrap();
-        let ci_bytes: Vec<u8> = img.to_bytes();
         let (ci_width, ci_height) = img.dimensions();
-        let img_type = match has_alpha(img){
-            true=>speedy2d::image::ImageDataType::RGBA,
-            false=>speedy2d::image::ImageDataType::RGB,
+        let has_alpha = has_alpha(&img);
+        
+        let ci_bytes = match has_alpha {
+            true => img.into_rgba8().into_raw(),
+            false => img.into_rgb8().into_raw()
+        };
+
+        let img_type = match has_alpha {
+            true => speedy2d::image::ImageDataType::RGBA,
+            false => speedy2d::image::ImageDataType::RGB,
         };
         let to_draw_image = match graphics.create_image_from_raw_pixels(
                 img_type,
@@ -42,55 +48,9 @@ impl WindowHandler for MyWindowHandler
         };
         graphics.draw_image((200.0, 0.0), &to_draw_image);
 
-        //loading image with true alpha channel gives(exit code: 0xc000041d), not caught by match statement
-        
-        // let img_2: DynamicImage = ImageReader::open("imgs/rust-logo.png").unwrap().decode().unwrap();
-        // let ci_bytes_2: Vec<u8> = img_2.to_bytes();
-        // let (ci_width_2, ci_height_2) =  img_2.dimensions();
-
-        // let img_type_2 = match has_alpha(img_2){
-        //     true=>speedy2d::image::ImageDataType::RGBA,
-        //     false=>speedy2d::image::ImageDataType::RGB,
-        // };
-        // let to_draw_image_2 = match graphics.create_image_from_raw_pixels(
-        //         img_type_2,
-        //         ImageSmoothingMode::NearestNeighbor,
-        //         Vector2::new(ci_width_2, ci_height_2),
-        //         &ci_bytes_2,
-        //     ){
-        //         Ok(img) => img,
-        //         Err(e) => panic!("Error creating image {}", e)
-        // };
-        // graphics.draw_image((0.0, 0.0), &to_draw_image_2);
     }
 }
 
-fn has_alpha(img: DynamicImage) -> bool{
-    dbg!(img.color().has_alpha());
+fn has_alpha(img: &DynamicImage) -> bool{
     img.color().has_alpha()
 }
-// let coords = self.pos;
-// let bytes = &self.data;
-// let img_width = self.width;
-// let img_height = self.height;
-// let img_type = self.img_data_type.clone();
-// dbg!(&img_type);
-// // let cont = container.();
-// let rect_pos = world_to_screen_2(coords, c_offset, c_scale);
-// let rect_pos_2 = world_to_screen_2(Vector2::new(coords.x+img_width as f32, coords.y+img_height as f32), c_offset, c_scale);
-
-// let to_draw_image = match graphics.create_image_from_raw_pixels(
-//     img_type,
-//     ImageSmoothingMode::NearestNeighbor,
-//     Vector2::new(img_width, img_height),
-//     &bytes,
-// ){
-//     Ok(img) => img,
-//     Err(e) => panic!("Error creating image {}", e)
-// };
-// let rect = speedy2d::shape::Rectangle::new(
-//     Vector2::new(rect_pos.x as f32, rect_pos.y as f32), 
-//     Vector2::new(rect_pos_2.x as f32, rect_pos_2.y as f32)
-// );
-
-// graphics.draw_rectangle_image(rect, &to_draw_image);
